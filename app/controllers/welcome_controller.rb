38bb812a9a27e30ae1a3fa92731e413216996ade
joeenscale <joseph+github@layershift.me>
2020-08-05 14:31:26 +0300

    check db connection without using a model

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,7 +6,10 @@ class WelcomeController < ApplicationController
     @ssl_on = request.ssl?
 
     # @db = Test.connection.present?
-    @db = true
+    ActiveRecord::Base.establish_connection # Establishes connection
+    ActiveRecord::Base.connection # Calls connection object
+
+    @db = ActiveRecord::Base.connected? 
 
     @all_done = @active_job_setup && @master_key_file && @ssl_on
 
